725533ad96a55a7cfb3b5b713dc283da97ee6da8
Michal Bencur <michal@michalbencur.com>
2025-10-19 15:48:32 +0200

    update LazyVim, added wezterm/ghostty

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,5 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps hereby
-
-vim.keymap.set("v", "<Leader>f", vim.lsp.buf.format, bufopts)
+-- Add any additional keymaps here
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,105 @@
+local function color_scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Catppuccin Mocha"
+	else
+		return "Catppuccin Mocha"
+	end
+end
+
+local wezterm = require("wezterm")
+
+local config = {}
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- config.font = wezterm.font("Anonymous Pro")
+config.font_size = 13.0
+config.color_scheme = color_scheme_for_appearance(wezterm.gui.get_appearance())
+config.window_background_opacity = 1.0
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+config.native_macos_fullscreen_mode = false
+
+config.leader = { key = "p", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+	{
+		key = "n",
+		mods = "LEADER",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "n",
+		mods = "LEADER|SHIFT",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "p",
+		mods = "LEADER",
+		action = wezterm.action.TogglePaneZoomState,
+	},
+
+	-- Navigate panes
+	{
+		key = "RightArrow",
+		mods = "CMD",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "LeftArrow",
+		mods = "CMD",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "UpArrow",
+		mods = "CMD",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "DownArrow",
+		mods = "CMD",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+
+	-- Send "CTRL-P" to the terminal when pressing CTRL-P, CTRL-P
+	{
+		key = "p",
+		mods = "LEADER|CTRL",
+		action = wezterm.action.SendKey({ key = "p", mods = "CTRL" }),
+	},
+
+	-- resize pane
+	{
+		key = "r",
+		mods = "LEADER",
+		action = wezterm.action.ActivateKeyTable({
+			name = "resize_pane",
+			one_shot = false,
+		}),
+	},
+}
+
+config.key_tables = {
+	resize_pane = {
+		{ key = "Escape", action = "PopKeyTable" },
+		{ key = "Enter", action = "PopKeyTable" },
+		{ key = "h", action = wezterm.action.AdjustPaneSize({ "Left", 5 }) },
+		{ key = "l", action = wezterm.action.AdjustPaneSize({ "Right", 5 }) },
+		{ key = "j", action = wezterm.action.AdjustPaneSize({ "Up", 5 }) },
+		{ key = "k", action = wezterm.action.AdjustPaneSize({ "Down", 5 }) },
+		{ key = "LeftArrow", action = wezterm.action.AdjustPaneSize({ "Left", 5 }) },
+		{ key = "RightArrow", action = wezterm.action.AdjustPaneSize({ "Right", 5 }) },
+		{ key = "UpArrow", action = wezterm.action.AdjustPaneSize({ "Up", 5 }) },
+		{ key = "DownArrow", action = wezterm.action.AdjustPaneSize({ "Down", 5 }) },
+	},
+}
+
+config.mouse_bindings = {
+	{
+		event = { Drag = { streak = 1, button = "Left" } },
+		mods = "SUPER",
+		action = wezterm.action.StartWindowDrag,
+	},
+}
+
+return config
